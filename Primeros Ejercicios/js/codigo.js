1970f2318de31bb3012e25b9a625a9aab2230d42
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-11-07 12:04:42 +0100

    listado arrays

---
+++ Primeros Ejercicios/js/codigo.js
@@ -0,0 +1,4 @@
+//Muestra el mensaje "Hola mundo" en una ventana emergente
+alert("Hola Mundo!");
+//Muestra el mensaje "Soy el primer script" en una ventana emergente 
+alert("Soy el primer script");
